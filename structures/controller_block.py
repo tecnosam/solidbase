b8c165861c3719caeb335a7d25c84ebf1a705968
tecnosam <ikabolo59@gmail.com>
2021-10-15 03:16:18 +0100

    added deleted attribute

--- structures/controller_block.py
+++ structures/controller_block.py
@@ -4,7 +4,7 @@ from .utils.array import array, Iterator
 
 class ControllerBlock:
 
-    def __init__( self, name, c_type, parent = 0, start = 0, end = 0, mime:str = "application/octet-stream" ):
+    def __init__( self, name, c_type, parent = 0, start = 0, end = 0, mime:str = None, id = None, deleted = False ):
 
         self._name = name
         self.c_type = c_type
@@ -15,18 +15,24 @@ class ControllerBlock:
 
         self.is_root = parent is None
 
+        self._deleted = deleted
+
+        self.id = id
+
     @staticmethod
     def load_dict( data:dict ):
         return ControllerBlock( **data )
 
     def to_dict(self):
         return {
+            "id": self.id,
             "name": self._name,
             "c_type": self.c_type,
             "parent": self.parent,
             "start": self.start,
             "mime": self.mime,
-            "end": self.end
+            "end": self.end,
+            "deleted": self._deleted
         }
 
     def rename ( self, new_name ):
@@ -37,13 +43,22 @@ class ControllerBlock:
     
     def refix_span( self, new_span:int ):
         self.end = self.start + ( new_span - 1 )
+    
+    def set_deleted( self, deleted ):
+        assert self.is_root != True
+        self._deleted = deleted
+
+    def get_deleted( self ) -> bool:
+        return self._deleted
 
     def __len__( self ):
 
         return (self.end - self.start) + 1
-    
+
     def __repr__(self) -> str:
         return f"<Block: \"{self.name}\", Type: {self.c_type}, SPAN: {self.span}>"
 
     name = property( get_name, rename )
     span = property( __len__, refix_span )
+
+    deleted = property( get_deleted, set_deleted )
